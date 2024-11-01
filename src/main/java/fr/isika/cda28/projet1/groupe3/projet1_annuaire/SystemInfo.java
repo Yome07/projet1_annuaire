3e7b4a2d542b8cb4950786c86999732236a2e0df
Guillaume Andreux <guillaume@andreux.fr>
2024-11-01 11:26:00 +0100

    first commit

---
+++ src/main/java/fr/isika/cda28/projet1/groupe3/projet1_annuaire/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.isika.cda28.projet1.groupe3.projet1_annuaire;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.isika.cda28.projet1.groupe3.projet1_annuaire {
+    requires javafx.controls;
+    exports fr.isika.cda28.projet1.groupe3.projet1_annuaire;
+}
