0aa0d08418d416314fa283014d6ee30c41633323
AnthonySk <anthony.statek@gmail.com>
2024-11-01 19:07:33 +0100

    add wireframeBasic.java

---
+++ src/main/java/fr/isika/cda28/projet1/groupe3/projet1_annuaire/WireframeBasic.java
@@ -0,0 +1,92 @@
+package fr.isika.cda28.projet1.groupe3.projet1_annuaire;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class WireframeBasic extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+//		---- COLORS ----
+//		F87A53 orange
+//		E6C767 sable
+//		898121 kaki
+//		4C4B16 kaki foncé
+
+		// 1. Title
+		primaryStage.setTitle("Stud'Index");
+
+		// 2. Pane Root
+		BorderPane root = new BorderPane();
+
+		// 3. Components
+		// 3.1 NavBarMenu
+		HBox navBarMenu = new HBox();
+		navBarMenu.setStyle("-fx-background-color: #898121;");
+		navBarMenu.setAlignment(Pos.CENTER);
+		Button home = new Button("Accueil");
+		Button handleUsers = new Button("Gestion des utilisateurs");
+		Button searchInterns = new Button("Recherche de stagiaires");
+		Button internsList = new Button("Liste des stagiaires");
+		Button addInterns = new Button("Ajout de stagiaire");
+
+		Button[] buttonNavBarMenu = { home, handleUsers, searchInterns, internsList, addInterns };
+		for (Button button : buttonNavBarMenu) {
+			button.setMinSize(140, 65);
+			navBarMenu.setMargin(button, new Insets(25));
+			button.setStyle("-fx-background-color: #E6C767; -fx-font-size: 16;");
+			button.setWrapText(true); // to center
+		}
+
+		navBarMenu.getChildren().addAll(home, handleUsers, searchInterns, internsList, addInterns);
+
+		// 3.2 Display area
+		AnchorPane areaDisplay = new AnchorPane();
+
+		Pane informationsDisplay = new Pane();
+		informationsDisplay.setMinWidth(630);
+		informationsDisplay.setStyle("-fx-background-color: #E6C767;");
+
+		// 3.3 Logo Stud'Index
+		Circle logo = new Circle(65);
+
+		// 3.4 disposition of the anchorPane
+		areaDisplay.getChildren().addAll(logo, informationsDisplay);
+		areaDisplay.setLeftAnchor(logo, 30.00);
+		areaDisplay.setTopAnchor(logo, 30.00);
+
+		areaDisplay.setTopAnchor(informationsDisplay, 15.00);
+		areaDisplay.setBottomAnchor(informationsDisplay, 71.00);
+		areaDisplay.setLeftAnchor(informationsDisplay, 189.00);
+
+		// 4. Add components to the pane
+		root.setTop(navBarMenu);
+		root.setCenter(areaDisplay);
+
+		// layout mac
+		root.setStyle("-fx-font-family: 'Proxima Nova'");
+		// 5. Set scene with the pane (root) and size
+		Scene scene = new Scene(root, 1000, 800);
+
+		// 6. Set the scene to the stage
+		primaryStage.setScene(scene);
+
+		// 7. Show stage
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module fr.isika.cda28.projet1.groupe3.projet1_annuaire {
     requires javafx.controls;
+	requires javafx.graphics;
     exports fr.isika.cda28.projet1.groupe3.projet1_annuaire;
 }
