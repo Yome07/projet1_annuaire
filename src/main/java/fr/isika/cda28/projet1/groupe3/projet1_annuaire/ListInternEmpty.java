e27c7a68cd3282eb746251873aaf73e33d89015b
Guillaume Andreux <guillaume@andreux.fr>
2024-11-03 14:37:58 +0100

    ajout bouton upload liste stagiaire

--- src/main/java/fr/isika/cda28/projet1/groupe3/projet1_annuaire/ListInternEmpty.java
+++ src/main/java/fr/isika/cda28/projet1/groupe3/projet1_annuaire/ListInternEmpty.java
@@ -1,6 +1,8 @@
 package fr.isika.cda28.projet1.groupe3.projet1_annuaire;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -13,11 +15,16 @@ public class ListInternEmpty extends WireframeBasic {
 	}
 
 	public void modScene() {
-
-		Label test = new Label("Hello John Doe");
-		test.setLayoutX(20); 
-		test.setLayoutY(20);
-		informationsDisplay.getChildren().add(test);
+		
+		Button uploadListInterns = new Button("Charger une listede statiaires");
+		uploadListInterns.setMinSize(140, 65);
+		uploadListInterns.setStyle("-fx-background-color: #F87A53; -fx-font-size: 16;");
+		uploadListInterns.setWrapText(true); // to center
+		
+//		Label test = new Label("Hello John Doe");
+		uploadListInterns.setLayoutX(20); 
+		uploadListInterns.setLayoutY(20);
+		informationsDisplay.getChildren().add(uploadListInterns);
 
 
 	}
